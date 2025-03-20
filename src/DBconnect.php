fa427e71259145afc5ba62f21bceb9c46b5d2838
Dany <71641218+DanyilT@users.noreply.github.com>
2025-03-20 03:49:57 +0000

    Create lists.css

--- src/DBconnect.php
+++ src/DBconnect.php
@@ -8,7 +8,7 @@ $config = require_once __DIR__ . '/../config.php';
 try {
     $dsn = "mysql:host=$config[host];dbname=$config[dbname]";
     $connection = new PDO($dsn, $config['user'], $config['password'], $config['options']);
-    echo 'DB connected';
+    // echo 'DB connected';
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
