768633e380a27ccc34e0e296631e8e5aeae0ed27
Dany <71641218+DanyilT@users.noreply.github.com>
2025-03-20 16:56:40 +0000

    Update get_following.php
    fix: that showed followers, not following

--- public/lib/profile/get_following.php
+++ public/lib/profile/get_following.php
@@ -7,7 +7,7 @@ if (!$userId) {
     exit();
 }
 
-$stmt = $connection->prepare("SELECT u.username FROM active_followers f JOIN users u ON f.follower_id = u.user_id WHERE f.following_id = ?");
+$stmt = $connection->prepare("SELECT u.username FROM active_followers f JOIN users u ON f.following_id = u.user_id WHERE f.follower_id = ?");
 $stmt->execute([$userId]);
 $following = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
