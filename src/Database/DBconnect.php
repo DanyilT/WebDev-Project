a95be303a2cbd54d2d2b91ef8e7e9ad5eb309056
Dany <71641218+DanyilT@users.noreply.github.com>
2025-04-09 22:51:02 +0100

    Update DBconnect.php
    Change dir

--- src/Database/DBconnect.php
+++ src/Database/DBconnect.php
@@ -3,7 +3,7 @@
  * DB connection
  */
 
-$config = require_once __DIR__ . '/../config.php';
+$config = require_once __DIR__ . '/../../config.php';
 
 try {
     $dsn = "mysql:host=$config[host];dbname=$config[dbname]";
