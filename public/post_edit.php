144986ab69d621f39adcc55e3e1feebafaea5aca
Dany <71641218+DanyilT@users.noreply.github.com>
2025-04-15 19:30:48 +0100

    Update post_new.php post_edit.php

--- public/post_edit.php
+++ public/post_edit.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['auth']['user_id'])) {
-    header('Location: auth.php#login');
+    header('Location: /auth.php#login');
     exit();
 }
 
--- public/post_new.php
+++ public/post_new.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['auth'])) {
-    header('Location: auth.php#login');
+    header('Location: /auth.php#login');
     exit();
 }
 
