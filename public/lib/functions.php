b40e810908b07a75ea708f61b2971c17d796c236
Dany <71641218+DanyilT@users.noreply.github.com>
2025-04-15 19:30:02 +0100

    Update functions.php

--- public/lib/functions.php
+++ public/lib/functions.php
@@ -1,7 +1,24 @@
 <?php
-function get_accounts($filePath) {
-    $accountsJson = file_get_contents($filePath);
-    return json_decode($accountsJson, true);
+/**
+ * @param DateInterval|bool $interval
+ * @return void
+ */
+function time_count_display(DateInterval|bool $interval): void {
+    if ($interval->y > 0) {
+        echo htmlspecialchars($interval->y . ' year' . ($interval->y > 1 ? 's' : '') . ' ago');
+    } elseif ($interval->m > 0) {
+        echo htmlspecialchars($interval->m . ' month' . ($interval->m > 1 ? 's' : '') . ' ago');
+    } elseif ($interval->d >= 7) {
+        echo htmlspecialchars(floor($interval->d / 7) . ' week' . (floor($interval->d / 7) > 1 ? 's' : '') . ' ago');
+    } elseif ($interval->d > 0) {
+        echo htmlspecialchars($interval->d . ' day' . ($interval->d > 1 ? 's' : '') . ' ago');
+    } elseif ($interval->h > 0) {
+        echo htmlspecialchars($interval->h . ' hour' . ($interval->h > 1 ? 's' : '') . ' ago');
+    } elseif ($interval->i > 0) {
+        echo htmlspecialchars($interval->i . ' minute' . ($interval->i > 1 ? 's' : '') . ' ago');
+    } else {
+        echo htmlspecialchars($interval->s . ' second' . ($interval->s > 1 ? 's' : '') . ' ago');
+    }
 }
 
 /**
