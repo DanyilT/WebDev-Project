ae60f1cfb5a8363482e2072cb05f03e683269eec
Dany <71641218+DanyilT@users.noreply.github.com>
2025-04-16 04:01:50 +0100

    Create auth.php
    Check logged as admin, if not ask for login as admin

---
+++ public/admin/lib/auth.php
@@ -0,0 +1,28 @@
+<?php
+// TODO: Don't store the admin password like this:
+const ADMIN_PASSWORD = 'admin-password'; // Hey everyone, look here! This is the admin password!
+$hashed_password = password_hash(ADMIN_PASSWORD, PASSWORD_DEFAULT);
+
+session_start();
+if (!isset($_SESSION['admin_authenticated'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['admin_password'])) {
+        if (password_verify($_POST['admin_password'], $hashed_password)) {
+            $_SESSION['admin_authenticated'] = true;
+            header('Location: ' . $_SERVER['PHP_SELF']);
+        } else {
+            $error = 'Invalid password';
+        }
+    } else {
+        $error = 'Please enter the admin password';
+    }
+
+    echo '<form method="POST">
+            <label for="admin_password">Admin Password:</label>
+            <input type="password" id="admin_password" name="admin_password" required>
+            <button type="submit">Submit</button>
+          </form>';
+    if ($error) {
+        echo '<p style="color:red;">' . htmlspecialchars($error) . '</p>';
+    }
+    exit();
+}
